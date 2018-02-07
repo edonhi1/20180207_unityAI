d64c587fd6119d250e6675bd5661189bd6c6e39f
edonhi <edonhi1@gmail.com>
2018-02-07 10:31:09 +0900

    testScript add

---
+++ AI_Programming/Assets/02.Scripts/ScriptForTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptForTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//이 스크립트는 깃허브 백업/클론을 테스트하기 위함입니다.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
