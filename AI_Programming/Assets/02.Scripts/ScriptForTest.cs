252ce7f4eb6fc32be87d5950620e38cb00e1470f
edonhi <edonhi1@gmail.com>
2018-02-07 10:36:45 +0900

    아침부터 뒤진다

--- AI_Programming/Assets/02.Scripts/ScriptForTest.cs
+++ AI_Programming/Assets/02.Scripts/ScriptForTest.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ScriptForTest : MonoBehaviour {
 
-	// Use this for initialization
+
 	void Start () {
 		//이 스크립트는 깃허브 백업/클론을 테스트하기 위함입니다.
+        //아침부터 졸려 뒤지겠다.
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		//Added this line for test cloning.
 	}
